f4301ff19a778d3cfd8ad9da4804d3a2e5d11e89
RKY <xraj2023@gmail.com>
2024-04-16 16:15:24 +0530

    prod changes bug

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,11 +1,14 @@
 // import React from "react";
 import Link from 'next/link';
 import HomeBody from './homebody';
+import Moon from "./moon";
+
 function home () {
     return (
     <>
         <h1>Home page</h1>
         <HomeBody />
+        <Moon />
     </>
     
 
