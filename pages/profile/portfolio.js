5f3ad7f1c232b504113e0e31304a6296a420a9cf
RKY <xraj2023@gmail.com>
2024-05-26 13:48:37 +0530

    testing

---
+++ pages/profile/portfolio.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Portfolio() {
+  return (
+    <div>
+      <div>Hey, I'm Raj. I'm looking for a SDE role for modern web development profile. Which company likely to work on new technology like React, tailwind, NextJS etc.</div>
+      <div>
+        {/* Image */}
+        <img src='' alt='' />
+        {/* Social media  infos */}
+        <div>
+
+        </div>
+        <div>
+          Summary
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Portfolio;
