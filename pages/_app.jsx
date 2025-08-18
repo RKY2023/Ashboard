66e4ef671838b8e7648141358187dd85b3caa355
raj <xraj2023@gmail.com>
2025-08-19 00:45:47 +0530

    Login

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,13 +1,21 @@
 import "@/styles/globals.css";
-import "../node_modules/bootstrap/dist/css/bootstrap.css";
-// import { Providers } from "../store/provider";
+import { Providers } from "../store/provider";
+import { ThemeProvider } from "@/components/theme-provider"
+import { Provider } from "react-redux";
 
 const App = ({ Component, pageProps }) => {
   return (
-    // <Providers>
     <>
-      <Component {...pageProps} />
-    {/* </Providers> */}
+     <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+      >
+      <Providers>
+        <Component {...pageProps} />
+      </Providers>
+    </ThemeProvider>
     </>
   );
 }
